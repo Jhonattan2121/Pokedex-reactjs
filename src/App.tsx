fcf208f92674f4912f0dc37a88cf643db6b54341
jhonattan ferri <jhoontutors@gmail.com>
2023-08-13 02:19:00 -0300

    styles global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import Header from "./components/Header";
 import Pokemons from "./pages/Pokemons";
 import Home from "./pages/Home";
 import Contato from "./pages/Contato";
+import  './Global.css';
 function App() {
   return (
     <Router>
