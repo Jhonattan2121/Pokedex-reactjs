d669afd5670e02f673071791140516022e927a5c
jhonattan ferri <jhoontutors@gmail.com>
2023-08-13 01:40:42 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Header from "./components/Header";
+import Pokemons from "./pages/Pokemons";
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+           <Header/>
+        <Routes>
+            <Route path="/Home" element={<Home/>} />
+            <Route path="/Pokemons" element={<Pokemons/>} />
+            <Route path="/Contato" element={<Contato/>} />
+
+        </Routes>
+    </Router>
   );
 }
 
